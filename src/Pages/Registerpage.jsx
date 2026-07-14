22f57ca57c9bff6e39867f6d88dd88bdd7c5ee3b
kaviyaja <jayakumarkaviya22@gmail.com>
2026-07-14 07:02:33 +0530

    Fixed Register page for React Router

--- src/Pages/Registerpage.jsx
+++ src/Pages/Registerpage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Registerpage() {
   const [form, setForm] = useState({
@@ -90,53 +91,123 @@ function Registerpage() {
 
   return (
     <div className="container my-5">
-
       <section className="register-container text-center">
-
         <h2>Create Your Account</h2>
 
         <p>Join our toy family and start shopping today!</p>
 
         <img
           src="https://images.unsplash.com/photo-1516627145497-ae6968895b74?w=300"
+          alt="Toy"
           width="220"
           height="220"
-          alt="Toy"
           className="img-fluid mb-4"
         />
 
-        <form onSubmit={handleSubmit} className="w-50 mx-auto text-start">
+        <form
+          onSubmit={handleSubmit}
+          className="w-50 mx-auto text-start"
+        >
+          <input
+            className="form-control mb-3"
+            name="fname"
+            placeholder="First Name"
+            value={form.fname}
+            onChange={handleChange}
+          />
 
-          <input className="form-control mb-3" name="fname" placeholder="First Name" value={form.fname} onChange={handleChange} />
+          <input
+            className="form-control mb-3"
+            name="lname"
+            placeholder="Last Name"
+            value={form.lname}
+            onChange={handleChange}
+          />
 
-          <input className="form-control mb-3" name="lname" placeholder="Last Name" value={form.lname} onChange={handleChange} />
+          <input
+            className="form-control mb-3"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={form.email}
+            onChange={handleChange}
+          />
 
-          <input className="form-control mb-3" type="email" name="email" placeholder="Email" value={form.email} onChange={handleChange} />
+          <input
+            className="form-control mb-3"
+            name="phone"
+            placeholder="Phone Number"
+            value={form.phone}
+            onChange={handleChange}
+          />
 
-          <input className="form-control mb-3" name="phone" placeholder="Phone Number" value={form.phone} onChange={handleChange} />
+          <input
+            className="form-control mb-3"
+            name="username"
+            placeholder="Username"
+            value={form.username}
+            onChange={handleChange}
+          />
 
-          <input className="form-control mb-3" name="username" placeholder="Username" value={form.username} onChange={handleChange} />
+          <input
+            className="form-control mb-3"
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={form.password}
+            onChange={handleChange}
+          />
 
-          <input className="form-control mb-3" type="password" name="password" placeholder="Password" value={form.password} onChange={handleChange} />
+          <input
+            className="form-control mb-3"
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            value={form.confirmPassword}
+            onChange={handleChange}
+          />
 
-          <input className="form-control mb-3" type="password" name="confirmPassword" placeholder="Confirm Password" value={form.confirmPassword} onChange={handleChange} />
-
-          <input className="form-control mb-3" type="date" name="dob" value={form.dob} onChange={handleChange} />
+          <input
+            className="form-control mb-3"
+            type="date"
+            name="dob"
+            value={form.dob}
+            onChange={handleChange}
+          />
 
           <div className="mb-3">
-
             <label className="me-3">
-              <input type="radio" name="gender" value="Male" onChange={handleChange} /> Male
+              <input
+                type="radio"
+                name="gender"
+                value="Male"
+                checked={form.gender === "Male"}
+                onChange={handleChange}
+              />{" "}
+              Male
             </label>
 
             <label className="me-3">
-              <input type="radio" name="gender" value="Female" onChange={handleChange} /> Female
+              <input
+                type="radio"
+                name="gender"
+                value="Female"
+                checked={form.gender === "Female"}
+                onChange={handleChange}
+              />{" "}
+              Female
             </label>
 
             <label>
-              <input type="radio" name="gender" value="Other" onChange={handleChange} /> Other
+              <input
+                type="radio"
+                name="gender"
+                value="Other"
+                checked={form.gender === "Other"}
+                onChange={handleChange}
+              />{" "}
+              Other
             </label>
-
           </div>
 
           <textarea
@@ -150,6 +221,7 @@ function Registerpage() {
 
           <div className="form-check mb-3">
             <input
+              id="terms"
               type="checkbox"
               name="terms"
               className="form-check-input"
@@ -157,23 +229,29 @@ function Registerpage() {
               onChange={handleChange}
             />
 
-            <label className="form-check-label">
+            <label
+              htmlFor="terms"
+              className="form-check-label"
+            >
               I agree to the Terms and Conditions
             </label>
           </div>
 
-          <button className="btn btn-success w-100">
+          <button
+            type="submit"
+            className="btn btn-success w-100"
+          >
             Register
           </button>
-
         </form>
 
         <p className="mt-3">
-          Already have an account? <a href="/login">Login Here</a>
+          Already have an account?{" "}
+          <Link to="/login">
+            Login Here
+          </Link>
         </p>
-
       </section>
-
     </div>
   );
 }
